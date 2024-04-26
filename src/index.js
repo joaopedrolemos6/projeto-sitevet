8cd7c0fe0cfdebbb9f14c5af896b378ae5ae1af0
GabrielbrandaoRP <gabrielhand15@gmail.com>
2024-04-26 15:17:40 -0300

    Criação das pastas de trabalho, criação da barra de navegação responsiva com menu hamburguer

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.mobileMenu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').getElementsByClassName('mobileMenuIcon')
+     } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').getElementsByClassName('mobileMenuIcon')
+    }
+}
